e6baf3c2d8cfc700eeb49539cdedde5a1aedcc2f
Johana Elizalde <njelizalde@uce.edu.ec>
2025-03-02 18:14:02 -0500

    feat(db): 🗄️ add database connection for viewing user profile microservice

---
+++ view-user-profile-microservice/db.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/lib/pq"
+)
+
+var DB *sql.DB
+
+func InitDB() {
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		dbHost, dbPort, dbUser, dbPassword, dbName)
+
+	var err error
+	DB, err = sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal("Error connecting to database: ", err)
+	}
+
+	if err = DB.Ping(); err != nil {
+		log.Fatal("Database not responding: ", err)
+	}
+
+	fmt.Println("Connected to the database successfully!")
+}
